38b211536858c48042ffec6d6489a0862de066d9
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-09 00:35:03 +0530

    add tests and code for neighbour list

--- grid/cellAddress.go
+++ grid/cellAddress.go
@@ -1,20 +1,22 @@
 package grid
 
+import "fmt"
+
 type CellAddress struct {
-	Row    uint32
-	Column uint32
+	Row    int
+	Column int
 }
 
 func (add CellAddress) NeighboursFor(grid Grid) []CellAddress {
+	fmt.Println(grid)
 	lastRowIndex, lastColumnIndex := grid.Rows()-1, grid.Columns()-1
 	var neighbours []CellAddress
-	for row := add.Row; row <= add.Row+1; row++ {
-		for column := add.Column; column <= add.Column+1; column++ {
+	for row := add.Row - 1; row <= add.Row+1; row++ {
+		for column := add.Column - 1; column <= add.Column+1; column++ {
 			if row < 0 || column < 0 || row > lastRowIndex || column > lastColumnIndex || (row == add.Row && column == add.Column) {
 				continue
 			}
 			neighbour := CellAddress{row, column}
-
 			neighbours = append(neighbours, neighbour)
 		}
 	}
--- grid/cellAddress_test.go
+++ grid/cellAddress_test.go
@@ -3,13 +3,15 @@ package grid
 import (
 	"reflect"
 	"testing"
+
+	"github.com/amancooks08/game-of-life/cell"
 )
 
 func TestNewCellAddresss(t *testing.T) {
 
 	type args struct {
-		row    uint32
-		column uint32
+		row    int
+		column int
 	}
 	type test struct {
 		name        string
@@ -36,6 +38,71 @@ func TestNewCellAddresss(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			got := CellAddress{tc.args.row, tc.args.column}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("Expected %v, got %v", tc.want, got)
+			}
+		})
+	}
+}
+
+func TestNeighboursFor(t *testing.T) {
+	type args struct {
+		grid Grid
+	}
+	type test struct {
+		name string
+		cell CellAddress
+		args args
+		want []CellAddress
+	}
+	tests := []test{
+		{
+			name: "Test neighbours for center cell to be eight.",
+			cell: CellAddress{1, 1},
+			args: args{
+				grid: Grid{
+					cells: [][]cell.Cell{
+						{cell.NewLiveCell(), cell.NewLiveCell(), cell.NewLiveCell()},
+						{cell.NewLiveCell(), cell.NewLiveCell(), cell.NewLiveCell()},
+						{cell.NewLiveCell(), cell.NewLiveCell(), cell.NewLiveCell()},
+					},
+				},
+			},
+			want: []CellAddress{{0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 2}, {2, 0}, {2, 1}, {2, 2}},
+		},
+		{
+			name: "Test neighbours for corner cell to be three.",
+			cell: CellAddress{0, 0},
+			args: args{
+				grid: Grid{
+					cells: [][]cell.Cell{
+						{cell.NewLiveCell(), cell.NewLiveCell(), cell.NewLiveCell()},
+						{cell.NewLiveCell(), cell.NewLiveCell(), cell.NewLiveCell()},
+						{cell.NewLiveCell(), cell.NewLiveCell(), cell.NewLiveCell()},
+					},
+				},
+			},
+			want: []CellAddress{{0, 1}, {1, 0}, {1, 1}},
+		},
+		{
+			name: "Test neighbours for edge cell to be five.",
+			cell: CellAddress{0, 1},
+			args: args{
+				grid: Grid{
+					cells: [][]cell.Cell{
+						{cell.NewLiveCell(), cell.NewLiveCell(), cell.NewLiveCell()},
+						{cell.NewLiveCell(), cell.NewLiveCell(), cell.NewLiveCell()},
+						{cell.NewLiveCell(), cell.NewLiveCell(), cell.NewLiveCell()},
+					},
+				},
+			},
+			want: []CellAddress{{0, 0}, {0, 2}, {1, 0}, {1, 1}, {1, 2}},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.cell.NeighboursFor(tc.args.grid)
 			if !reflect.DeepEqual(got, tc.want) {
 				t.Fatalf("Expected %v, got %v", tc.want, got)
 			}

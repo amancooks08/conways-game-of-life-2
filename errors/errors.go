e2247e86faaf7d46f1f87cf79dfaffc5d4b31b99
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-08 13:23:59 +0530

    get assert package

--- errors/errors.go
+++ errors/errors.go
@@ -5,5 +5,5 @@ import "errors"
 var (
 	ErrInvalidRowAddress    = errors.New("invalid row address")
 	ErrInvalidColumnAddress = errors.New("invalid column address")
-	
+	ErrInvalidNeighbours	= errors.New("invalid number of neighbours")
 )

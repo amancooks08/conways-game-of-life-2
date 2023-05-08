cabf0b339654ae697ab35ab85f7a2fac40b1babd
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-08 17:45:49 +0530

    remove cellAddress errors

--- errors/errors.go
+++ errors/errors.go
@@ -3,8 +3,6 @@ package errors
 import "errors"
 
 var (
-	ErrInvalidRowAddress    = errors.New("invalid row address")
-	ErrInvalidColumnAddress = errors.New("invalid column address")
 	ErrInvalidNeighbours	= errors.New("invalid number of neighbours")
 	ErrInvalidRows			= errors.New("invalid number of rows")
 	ErrInvalidColumns		= errors.New("invalid number of columns")
